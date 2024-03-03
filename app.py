1f172f8b3594cd3776aa47bd01c0f47b8a304bd7
telekinetic-0004 <jayminschandaria@gmail.com>
2024-03-03 23:27:31 +0530

    commit message

--- app.py
+++ app.py
@@ -96,4 +96,7 @@ def upload():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
